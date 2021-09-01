3346ebf78ca52c1ba914b48dae4e622cbf4416c6
Matias <35298560+matiasrj@users.noreply.github.com>
2021-09-01 16:55:11 -0300

    Update Seccion3.js

--- app/components/main/Seccion3.js
+++ app/components/main/Seccion3.js
@@ -27,16 +27,16 @@ export  function Seccion3 (){
     $seccion.id='seccion3';
     $parrafo.innerHTML=`
     <p>Si has llegado hasta aqui, <br><br>
-    quizas sea de tu interes contactarme.
-    Si tienes alguna consulta, comentario, o estás en búsqueda de iniciar un proyecto, ó simplemente algun
+    quizás sea de tu interés contactarme.
+    Si tienes alguna consulta, comentario, o estás en búsqueda de iniciar un proyecto, ó simplemente algún
     tips o fuentes desde los cuales aprendí a desarrollar este y otros sitios, no dudes en comunicarte. A continuación
     te dejo un formulario.  <br></p>
     
-    <p> Nota: En unos dias mas, voy a incorporar a este sitio los links adicionales para que 
+    <p> Nota: En unos dias más, voy a incorporar a este sitio los links adicionales para que 
     puedas contactarme via LinkedIn, Github, y demas... <br><br> </p> 
 
 
-    <p> Mientras tanto, espero que tengas un buen dia y gracias por tu interés... <br> </p> 
+    <p> Mientras tanto, espero que tengas un buen día y gracias por tu interés... <br> </p> 
 
 
    `
